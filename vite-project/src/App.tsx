79936e0060419cbfe05bdaf67d227e20d1c145a5
rofun <joufang.w@gmail.com>
2023-10-06 04:17:08 +0800

    Feat: upslash images

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,23 +1,26 @@
 import { FormEvent, useState } from "react";
 import "./App.css";
 import OpenAI from "openai";
+import axios from "axios";
 
 function App() {
   const [prompt, setPrompt] = useState("");
   const [result, setResult] = useState("");
+  const [image, setImage] = useState("");
   const openai = new OpenAI({
     apiKey: import.meta.env.VITE_CHAT_API_KEY,
     dangerouslyAllowBrowser: true,
   });
+  const IMAGE_URL = "https://api.unsplash.com/search/photos";
 
-  let chatCompletion = async () => {
+  const chatCompletion = async () => {
     await openai.chat.completions
       .create({
         model: "gpt-3.5-turbo",
         messages: [
           {
             role: "user",
-            content: `Please provide 5 contemporary architecture of this city ${prompt}, but just provide the names of the architectures without any details, and return them in an array format.`,
+            content: `Please provide the name of one famous contemporary architecture of this city ${prompt}, but just provide the name of the architecture without any details, only the name of the architecture.`,
           },
         ],
       })
@@ -25,11 +28,26 @@ function App() {
         const data = res.choices[0].message.content;
         if (data) {
           setResult(data);
+          getImages(data);
         }
       })
       .catch((error: Error) => {
         console.log(error.message);
       });
+  };
+
+  const getImages = async (archName: string) => {
+    try {
+      const images = await axios.get(
+        `${IMAGE_URL}?query=${archName}&page=1&per_page=5&client_id=${
+          import.meta.env.VITE_UNSPLASH_API_KEY
+        }`
+      );
+      setImage(images.data.results[0].urls.small);
+      return;
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleSubmit = (e: FormEvent) => {
@@ -48,6 +66,7 @@ function App() {
         <button type="submit">Search</button>
       </form>
       <p>{result}</p>
+      <img src={image} />
     </>
   );
 }
